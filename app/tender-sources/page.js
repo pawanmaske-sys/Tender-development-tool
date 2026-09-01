7ae8f6fe15ce196aa7d702b19a98425e58bc1e49
pawanmaske-sys <pawan.maske@satgurutravel.com>
2026-09-01 13:44:33 +0530

    Create page.js

---
+++ app/tender-sources/page.js
@@ -0,0 +1,10 @@
+import ModulePlaceholder from "../components/ModulePlaceholder";
+
+export default function TenderSourcesPage() {
+  return (
+    <ModulePlaceholder
+      title="Tender Sources"
+      subtitle="Tender portal and opportunity source directory"
+    />
+  );
+}
